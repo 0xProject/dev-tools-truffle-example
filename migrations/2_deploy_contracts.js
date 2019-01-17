ef39678fe986b324edb45be4853a9e2e4702753f
Leonid Logvinov <logvinov.leon@gmail.com>
2019-01-17 14:29:53 +0100

    Remove all

--- migrations/2_deploy_contracts.js
+++
@@ -1,7 +0,0 @@
-const SafeMath = artifacts.require("SafeMath");
-const MetaCoin = artifacts.require("MetaCoin");
-
-module.exports = function(deployer) {
-  deployer.deploy(SafeMath);
-  deployer.deploy(MetaCoin);
-};
